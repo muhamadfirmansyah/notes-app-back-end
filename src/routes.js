93dd2072d123fcd6408e8b2141477c0932bf9717
Fnwrkio <firmansyah.assignment@gmail.com>
2022-11-11 05:54:27 +0700

    inital commit

---
+++ src/routes.js
@@ -0,0 +1,33 @@
+const {
+  addNoteHandler, getAllNotes, getNoteById, editNoteByIdHandler, deleteNoteByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotes,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteById,
+  },
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+module.exports = routes;
